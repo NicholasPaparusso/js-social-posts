9e49820d4f634d2ba2da41495036982e9a8a5f23
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-28 11:13:34 +0200

    [add]  correction during lesson

--- main.js
+++ main.js
@@ -59,6 +59,10 @@ const posts = [
 // Elementi Dom
 const container = document.getElementById('container');
 
+// global var
+const userLikes = []
+let likesCounter = 0;
+// logic & functions
 function init(){
     reset();
     posts.forEach((post)=>{
@@ -69,15 +73,98 @@ function init(){
 init();
 
  function getPost(post){
- const {id, content, media, likes, created} = post;
- let authorName = posts[author.image]
-//  let authorImage = posts.author.image;
- console.log(authorName);
- return
+ const {id, content, media, likes, created, author} = post;
+timeAgo(post);
+FormDate(post);
+fallback(post);
+ return`
+ <div class="post">
+ <div class="post__header">
+     <div class="post-meta">                    
+         <div class="post-meta__icon">
+         ${author.image ? getImageProfile(author) : getProfileDefault(author)}
+         </div>
+         <div class="post-meta__data">
+             <div class="post-meta__author">${author.name}</div>
+             <div class="post-meta__time">${calculatedMonth} mesi fa , data creazione: ${formattedDate}</div>
+         </div>                    
+     </div>
+ </div>
+ <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+ <div class="post__image">
+     <img src="${media}" alt="">
+ </div>
+ <div class="post__footer">
+     <div class="likes js-likes">
+         <div class="likes__cta">
+             <a class="like-button js-like-button"  onclick="likeFunction(this)" href="#${id}" data-postid="${id}">
+                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                 <span class="like-button__label">Mi Piace</span>
+             </a>
+         </div>
+         <div class="likes__counter">
+             Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
+         </div>
+     </div> 
+ </div>            
+</div>
 
+</div>
+ `
+ 
 }
 
 
 function reset(){
  container.innerHTML ='';
+};
+
+function timeAgo(post){
+    const{created} = post;
+    const monthOfCreated = created.slice(5,7)
+    const yearOfCreated = created.slice(0,4)
+    const getTime = new Date;
+    const year = getTime.getFullYear();
+    const month = getTime.getMonth()+1;
+    if(year > yearOfCreated){
+         calculatedMonth =  (year - yearOfCreated)*12 +  month  - monthOfCreated; 
+    }else{
+         calculatedMonth = month - monthOfCreated; 
+    }
+};
+
+function FormDate(post){
+    const{created}= post;
+    const arrayOfCreated = [...created];
+    const joinArray = arrayOfCreated.join("").split("-")
+    const reversedArray = joinArray.reverse()
+     reversedArray.splice(-1,0, "/");
+     reversedArray.splice(-3,-2, "/");
+    return formattedDate = reversedArray.toString().replace(/[^0-9./]+/g,"")
+};
+
+
+function fallback(post){
+    const {author} = post;
+    const arrayOfName = author.name.split(' ');
+    return fallbackName = arrayOfName[0][0] + arrayOfName[1][0];
+};
+
+
+function likeFunction(element){
+  element.classList.toggle('clicked');
+};
+
+
+function getImageProfile(author){
+    const {image} = author;
+    return `<img class="profile-pic" src="${image}" alt="${name}">`
+};
+
+function getProfileDefault(author){
+    return `<span class="profile-pic-default">${fallbackName}<span>`
+};
+
+function isPostLiked(id){
+    return userLikes.includes(id);
 };
