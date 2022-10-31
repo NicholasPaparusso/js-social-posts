5d9ad51ff82fbe91c2227c7875f3a27fdd1d4514
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-31 11:04:26 +0100

    [change] corrections

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const posts = [
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
-            "name": "Luca Formicola",
+            "name": "Luca Formicola De Formicoli",
             "image": null
         },
         "likes": 56,
@@ -60,11 +60,13 @@ const posts = [
 const container = document.getElementById('container');
 
 // global var
-const userLikes = []
-let likesCounter = 0;
+const userLikes = [1,3,4]
+
 // logic & functions
+
 function init(){
     reset();
+
     posts.forEach((post)=>{
         container.innerHTML+= getPost(post)
     })
@@ -74,9 +76,8 @@ init();
 
  function getPost(post){
  const {id, content, media, likes, created, author} = post;
-timeAgo(post);
-FormDate(post);
-fallback(post);
+ let timeDiff = "";
+
  return`
  <div class="post">
  <div class="post__header">
@@ -86,7 +87,7 @@ fallback(post);
          </div>
          <div class="post-meta__data">
              <div class="post-meta__author">${author.name}</div>
-             <div class="post-meta__time">${calculatedMonth} mesi fa , data creazione: ${formattedDate}</div>
+             <div class="post-meta__time">${timeAgo(created,timeDiff)} , data creazione: ${FormDate(created)}</div>
          </div>                    
      </div>
  </div>
@@ -97,7 +98,7 @@ fallback(post);
  <div class="post__footer">
      <div class="likes js-likes">
          <div class="likes__cta">
-             <a class="like-button js-like-button"  onclick="likeFunction(this)" href="#${id}" data-postid="${id}">
+             <a class="like-button js-like-button ${isPostLiked(id) ? 'like-button--liked' : ' '}" href="#" data-postid="${id}">
                  <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                  <span class="like-button__label">Mi Piace</span>
              </a>
@@ -119,51 +120,94 @@ function reset(){
  container.innerHTML ='';
 };
 
-function timeAgo(post){
-    const{created} = post;
-    const monthOfCreated = created.slice(5,7)
-    const yearOfCreated = created.slice(0,4)
-    const getTime = new Date;
-    const year = getTime.getFullYear();
-    const month = getTime.getMonth()+1;
-    if(year > yearOfCreated){
-         calculatedMonth =  (year - yearOfCreated)*12 +  month  - monthOfCreated; 
-    }else{
-         calculatedMonth = month - monthOfCreated; 
+function timeAgo(date, diffTime){
+   const date1 = new Date(date);;
+   const date2= new Date();
+   const timeDifference = Math.abs(date1 - date2);
+   console.log(timeDifference);
+   console.log(date1);
+   console.log(date2);
+    if(timeDifference < 60000 ){
+    diffTime = "Qualche secondo fa";
+    }else if(timeDifference <= 3540000){
+        diffTime = `Pubblicato ${Math.floor(timeDifference / (1000 * 60))} minuti fa`;
+    }else if(timeDifference < 86400000){
+        diffTime = `Pubblicato ${Math.floor(timeDifference / (1000 * 3600))} ore fa`;
+    }else if(timeDifference <= 2592000000){
+        diffTime = `Pubblicato ${Math.floor(timeDifference / (1000 * 3600 * 24))} giorni fa`;
+    }else if(timeDifference <= 31557600000){
+        diffTime = `Pubblicato ${Math.floor(timeDifference / 2629800000)} mesi fa`;
+    }else if(timeDifference <= 63115200000){
+        diffTime = `Pubblicato ${Math.floor(timeDifference / 31557600000)} anno e ${Math.floor((timeDifference - 31557600000)  / 2629800000)} mesi fa`;
+    }else{ diffTime = `Pubblicato ${Math.floor(timeDifference / 63115200000)} anni e ${Math.floor((timeDifference - 63115200000)  / 2629800000)} mesi fa`;
+
     }
-};
-
-function FormDate(post){
-    const{created}= post;
-    const arrayOfCreated = [...created];
-    const joinArray = arrayOfCreated.join("").split("-")
-    const reversedArray = joinArray.reverse()
-     reversedArray.splice(-1,0, "/");
-     reversedArray.splice(-3,-2, "/");
-    return formattedDate = reversedArray.toString().replace(/[^0-9./]+/g,"")
+    return diffTime;
 };
 
 
-function fallback(post){
-    const {author} = post;
-    const arrayOfName = author.name.split(' ');
-    return fallbackName = arrayOfName[0][0] + arrayOfName[1][0];
-};
-
-
-function likeFunction(element){
-  element.classList.toggle('clicked');
+function FormDate(date){
+    return date.split('-').reverse().join('/');
 };
 
 
 function getImageProfile(author){
-    const {image} = author;
+    const {image, name} = author;
     return `<img class="profile-pic" src="${image}" alt="${name}">`
 };
 
 function getProfileDefault(author){
-    return `<span class="profile-pic-default">${fallbackName}<span>`
+    const{name} = author;
+    let initials = "";
+    const nameParts = name.split(' ');
+    nameParts.forEach( part => {
+    initials += part[0];
+    })
+
+    
+    return `
+    <div class="profile-pic-default">
+        <span>${initials}</span>
+    </div>
+    `
 };
+
+
+
+const likesButtons = document.querySelectorAll('.like-button');
+
+
+likesButtons.forEach( likeButton => {
+   
+    likeButton.addEventListener('click', function(event){
+      
+        event.preventDefault();
+       
+        const postId = parseInt(this.getAttribute('data-postid'));
+
+        const counterDisplay = document.getElementById('like-counter-' + postId);
+        let likes = parseInt(counterDisplay.innerText);
+      
+
+        if(this.classList.contains('like-button--liked')){
+            this.classList.remove('like-button--liked');
+ 
+            counterDisplay.innerText = --likes;
+        }else{
+            this.classList.add('like-button--liked');
+
+            counterDisplay.innerText = ++likes;
+        }
+        
+
+        const likedPost = posts.filter( (post) => post.id === postId )
+ 
+        likedPost[0].likes = likes;
+
+    })
+
+} )
+
 
 function isPostLiked(id){
     return userLikes.includes(id);
